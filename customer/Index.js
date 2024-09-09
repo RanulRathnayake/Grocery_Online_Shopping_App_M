1cbb9d55f970015a4593ca92c1a5bd4ffd01aba6
RanulRathnayake <rathnayakerg.20@uom.lk>
2024-09-09 06:09:21 +0530

    customer database

--- customer/Index.js
+++ customer/Index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const { databaseConnection } = require('./src/database/connection.js');
 
 const app = express();
 
-app.use(express.json());
+async function startServer() {
+    try {
+        await databaseConnection(); 
 
-app.get('/', (req, res,next) => {
-    return res.status(200).json({ message: 'customer service' })
-});
+        app.use(express.json());
 
-app.listen(8001, () => {
-    console.log('Server is running on port 8001');
-});
+        app.get('/', (req, res, next) => {
+            return res.status(200).json({ message: 'customer service' });
+        });
+
+        app.listen(8001, () => {
+            console.log('Server is running on port 8001');
+        });
+    } catch (error) {
+        console.error('Failed to connect to the database:', error);
+        process.exit(1); // Exit the process with a failure code
+    }
+}
+
+startServer(); // Call the async function to start the server
--- customer/src/database/connection.js
+++ customer/src/database/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const { DB_URL } = require('../config');
-
-module.exports = async() => {
+/* const { DB_URL } = require('../config');
+ */
+const  DB_URL  = 'mongodb+srv://root:1234@cluster10.hcebh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster10'
+async function databaseConnection() {
 
     try {
         await mongoose.connect(DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
         });
         console.log('Db Connected');
         
@@ -16,5 +16,8 @@ module.exports = async() => {
         console.log(error);
         process.exit(1);
     }
- 
-};
+
+    console.log('Database connected successfully');
+}
+
+module.exports = { databaseConnection };
